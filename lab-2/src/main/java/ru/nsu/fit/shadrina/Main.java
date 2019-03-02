8e5fa5c3b84daacc0180a06452b69a852f1b032d
Anastasiya Shadrina <a.shadrina5@mail.ru>
2019-03-02 14:10:05 +0700

    Add colors

--- lab-2/src/main/java/ru/nsu/fit/shadrina/Main.java
+++ lab-2/src/main/java/ru/nsu/fit/shadrina/Main.java
@@ -4,8 +4,16 @@ import picocli.CommandLine;
 import ru.nsu.fit.shadrina.sm.FiniteStateMachine;
 
 public class Main {
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+
     public static void main(String[] args) {
         FiniteStateMachine sm = CommandLine.populateCommand(new FiniteStateMachine(), args);
-        System.out.println("Input string " + (sm.parse() ? "can" : "cannot") + " be parsed");
+        System.out.println("Input string " +
+                (sm.parse()
+                        ? ANSI_GREEN + "can" + ANSI_RESET
+                        : ANSI_RED + "cannot" + ANSI_RESET)
+                + " be parsed");
     }
 }
